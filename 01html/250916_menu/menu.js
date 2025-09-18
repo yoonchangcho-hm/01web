5d971ebf240c99db8ecb136d3e1e2a7d706f2302
yoonchangcho-hm <yoonchangcho@gmail.com>
2025-09-18 17:56:47 +0900

    first

---
+++ 01html/250916_menu/menu.js
@@ -0,0 +1,52 @@
+const menu = [
+  {
+    //menu[0].menu
+    menu: 'company',
+    url: 'http://www.naver.com',
+    sub: false,
+    submenu: [],
+  },
+  {
+    //menu[1].menu
+    menu: 'product',
+    url: 'http://www.daum.net',
+    sub: true,
+    submenu: [
+      {
+        menu: 'p01_01',
+        url: '#',
+      },
+      {
+        menu: 'p01_02',
+        url: '#',
+      },
+    ],
+  },
+  {
+    menu: 'oline',
+    url: '#',
+    sub: false,
+    submenu: [],
+  },
+  {
+    menu: 'community',
+    url: '#',
+    sub: true,
+    submenu: [
+      {
+        menu: 'c01_01',
+        url: '#',
+      },
+      {
+        menu: 'c01_02',
+        url: '#',
+      },
+    ],
+  },
+  {
+    menu: 'board',
+    url: '#',
+    sub: false,
+    submenu: [],
+  },
+];
