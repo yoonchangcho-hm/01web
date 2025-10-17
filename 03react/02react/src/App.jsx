7a6299c5db5633f7d9c7b8e5f9f0753e87a1fc1e
Your Name <your.email@example.com>
2025-10-17 11:41:31 +0900

    251017_1130

--- 03react/02react/src/App.jsx
+++ 03react/02react/src/App.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/state/Button';
 
 function App() {
+  const test = 3;
+  const [count, setCount] = useState(1);
+  const [view, setView] = useState(false);
+  // const [count] = useState([
+  //   { city: '서울', cont: 'test' },
+  //   { city: '부산', cont: 'test' },
+  // ]);
+  // const [count] = useState(['서울', '부산']);
+  // const [allCity] = useState([{ city: '서울', cont: 'test' }]);
+  // console.log(count[0]);
+  // console.log(count[0].city);
+
+  function han() {
+    setCount(count + 1);
+  }
+
+  const han1 = function () {
+    setCount(count + 1);
+  };
+
+  const han2 = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div>
-      <h3>state</h3>
-      <Button title="글쓰기" color="red" width="200"></Button>
-      <Button></Button>
-    </div>
+    <>
+      <div>
+        <h3>state</h3>
+        <button onClick={han2}>{count}</button>
+        {/* <button onClick={()=>{setCount(count+1)}}>{count}</button> */}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          {count}
+        </button>
+        <hr />
+        {test}
+        <Button title="글쓰기"></Button>
+        <Button title="글수정" color="red"></Button>
+        <Button title="글리스트" color="blue"></Button>
+
+        {/* <a href="" className="btn btn-primary"></a> */}
+      </div>
+    </>
   );
 }
 
--- 03react/02react/src/components/state/Button.jsx
+++ 03react/02react/src/components/state/Button.jsx
@@ -4,11 +4,20 @@ function Button({ title = '글작성', color = 'skyblue', width = '100' }) {
   //   console.log(props);
   console.log(title, color, width);
   const btnStyle = {
-    width: `${width}px`,
+    // width: `${width}px`,
+    display: 'inline-block',
     background: color,
+    borderRadius: '20px',
+    padding: '5px 16px',
+    color: 'white',
+    cursor: 'pointer',
   };
 
-  return <div style={btnStyle}>{title}</div>;
+  return (
+    <>
+      <div style={btnStyle}>{title}</div>
+    </>
+  );
 }
 
 export default Button;
