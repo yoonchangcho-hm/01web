b43bf4723ea9d39e0e28c969e3d3e12db03f6cf5
Your Name <your.email@example.com>
2025-10-29 12:24:25 +0900

    251029_1220

--- 03react/02react/src/components/pages/board/ViewComp.jsx
+++ 03react/02react/src/components/pages/board/ViewComp.jsx
@@ -1,24 +1,36 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-function ViewComp({ API }) {
-  // const param = useParams();
-  // console.log(param);
-  const { num } = useParams();
-  console.log(num);
+function ViewComp() {
+  const { num } = useParams(); // URL 파라미터
+  const [post, setPost] = useState(null); // 글 데이터 저장
 
   useEffect(() => {
-    const postFatch = async () => {
-      const { data } = await axios.get(`${API}/posts/${num}`);
-      console.log(data);
+    const fetchPost = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${num}`
+        );
+        // userId를 글번호(num)와 같게 강제로 맞춤
+        setPost({ ...data, userId: Number(num) });
+      } catch (error) {
+        console.error('데이터 불러오기 실패:', error);
+      }
     };
-    postFatch();
-  }, []);
+    fetchPost();
+  }, [num]);
+
   return (
     <div>
       <h3>글보기 / {num}</h3>
-      <div></div>
+      {post && (
+        <div>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+          <small>작성자 ID: {post.userId}</small>
+        </div>
+      )}
     </div>
   );
 }
