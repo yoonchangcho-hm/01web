7fe77ce637a1e024f88d942f8afa57dd1fb68f2a
Your Name <your.email@example.com>
2025-11-12 11:39:12 +0900

    251112_1139

--- 03react/02react/src/App.jsx
+++ 03react/02react/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function App() {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('이미지 파일업로드');
+  const [text, setText] = useState('');
 
   const fileChangeHandler = (e) => {
     console.log(e.target.files[0]);
@@ -15,25 +16,38 @@ function App() {
     e.preventDefault();
     const formData = new FormData();
     formData.append('image', file);
+    formData.append('test', text);
 
     console.log(formData);
 
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value);
+    }
     // alert('전송');
+
+    // axios.post("경로",formData)
   };
 
   return (
     <div>
       <h3>파일업로드</h3>
+
       <form onSubmit={submitHandler}>
         <div>
           <label htmlFor="file">{fileName}</label>
           <br />
           <input type="file" id="file" onChange={fileChangeHandler} multiple />
         </div>
-        {/* <div>
+        <div>
           <label htmlFor="">name</label>
-          <input type="text" id="testinput" />
-        </div> */}
+          <input
+            type="text"
+            id="testinput"
+            onChange={() => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
         <button type="submit">전송</button>
       </form>
     </div>
